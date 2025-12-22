0099153ebf9db4df570cae251799f8f2d58036d4
Jack Goodman <jacksgoodman3@gmail.com>
2025-12-22 11:14:34 +0000

    Schema validation test (8 unit tests now implemented)

---
+++ backend/tests/unit/orderItem.model.test.js
@@ -0,0 +1,13 @@
+import { jest } from "@jest/globals";
+import OrderItem from "../../src/models/OrderItem.js";
+
+describe("OrderItem model", () => {
+  test("rejects quantity less than 1", async () => {
+    const item = new OrderItem({
+      quantity: 0,
+      price: 5
+    });
+
+    await expect(item.save()).rejects.toThrow();
+  });
+});
